b6d1577ac562df113530537139513f76ccd51b19
FazioNico <contact@nicolasfazio.ch>
2016-10-17 13:14:05 +0200

    upd style ...

--- footer.php
+++ footer.php
@@ -12,14 +12,6 @@
 ?>
 
 	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nomades-projets' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'nomades-projets' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'nomades-projets' ), 'nomades-projets', '<a href="http://nicolasfazio.ch" rel="designer">Nicolas Fazio</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -23,39 +23,43 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'nomades-projets' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding row">
-			<div class="col-sm-4">
-				<h1 class="site-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						TRAVAUX DE PROJET
-						<?php //bloginfo( 'name' ); ?>
-					</a>
-				</h1>
-			</div>
-			<div class="col-sm-4">
-				<div id="header-logo">
-					<img src="<?php echo get_template_directory_uri();?>/src/img/logo-nomades-small-gray.png" alt="logo Nomades" />
-					<p>
-						nomades.ateliers
-					</p>
+		<div class="site-branding">
+			<div class="row">
+				<div class="col-sm-4">
+					<h1 class="site-title">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+							TRAVAUX DE PROJET
+							<?php //bloginfo( 'name' ); ?>
+						</a>
+					</h1>
 				</div>
-
+				<div class="col-sm-4">
+					<div id="header-logo">
+						<img src="<?php echo get_template_directory_uri();?>/src/img/logo-nomades-small-gray.png" alt="logo Nomades" />
+						<p>
+							nomades.ateliers
+						</p>
+					</div>
+				</div>
+				<div class="header-search-form col-sm-4">
+					<?php get_search_form();?>
+				</div>
 			</div>
 
-			<div class="header-search-form col-sm-4">
-				<?php get_search_form();?>
-			</div>
-			<div class="col-xs-12">
-				<hr class="separation-header" />
-			</div>
+			<div class="row">
+	
+				<div class="col-xs-12">
+					<hr class="separation-header" />
+				</div>
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'nomades-projets' ); ?></button>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+				</nav><!-- #site-navigation -->
+			</div><!-- .row -->
+
+
 		</div><!-- .site-branding row-->
 
-		<div class="row">
-			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'nomades-projets' ); ?></button>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-			</nav><!-- #site-navigation -->
-		</div><!-- .row -->
 
 	</header><!-- #masthead -->
 
