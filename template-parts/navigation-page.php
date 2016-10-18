1b831db8cb8f01f1b5621d85d24fca21150a5ff7
FazioNico <contact@nicolasfazio.ch>
2016-10-18 10:36:59 +0200

    styling navigation menu ...

--- template-parts/navigation-page.php
+++ template-parts/navigation-page.php
@@ -1,10 +1,7 @@
 <nav id="site-navigation" class="main-navigation" role="navigation">
   <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'nomades-projets' ); ?></button>
   <div class="row">
-    <div class="col col-sm-3">
-
-    </div>
-    <div class="col col-sm-9">
+    <div class="col col-sm-12">
         <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
     </div>
   </div>
