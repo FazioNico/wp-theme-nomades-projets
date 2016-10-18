44fffbec1d76acdb68901a999e6011aa44655f65
FazioNico <contact@nicolasfazio.ch>
2016-10-18 09:49:34 +0200

    woriking with sass

--- header.php
+++ header.php
@@ -47,14 +47,11 @@
 			</div>
 
 			<div class="row">
-	
+
 				<div class="col-xs-12">
 					<hr class="separation-header" />
 				</div>
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'nomades-projets' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav><!-- #site-navigation -->
+
 			</div><!-- .row -->
 
 
