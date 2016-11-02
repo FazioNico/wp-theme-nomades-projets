0d427200e5fc49b5b53f5e3b13e206e991ea65f9
FazioNico <contact@nicolasfazio.ch>
2016-11-02 10:03:59 +0100

    add confirmation alert to deleted a img attachment

--- js/main.js
+++ js/main.js
@@ -12,21 +12,28 @@ document.addEventListener("DOMContentLoaded", function(event) {
       console.log('project edtit ->')
       document.getElementById('attachementImg').addEventListener('click', (event)=>{
         event.preventDefault();
-        let imgID = event.target.dataset.id
-        console.log(imgID)
-        console.log(document.URL+"&deleteImgAtt="+imgID)
-        jQuery.ajax({
-            type: 'post',
-            url: ajaxurl,
-            data: {
-                action: 'delete_attachment',
-                att_ID: imgID,
-                post_type: 'attachment'
-            },
-            success: function( html ) {
-                alert( html );
-            }
-        });
+
+        let r = confirm("Supprimer l'image?!\n");
+        if (r == true) {
+          let img = event.target
+          let imgID = event.target.dataset.id
+          console.log(imgID)
+          console.log(document.URL+"&deleteImgAtt="+imgID)
+          jQuery.ajax({
+              type: 'post',
+              url: ajaxurl,
+              data: {
+                  action: 'delete_attachment',
+                  att_ID: imgID,
+                  post_type: 'attachment'
+              },
+              success: function( html ) {
+                  //alert( html );
+                  jQuery(img).remove();
+              }
+          });
+        }
+
         //document.location = document.URL+"&deleteImgAtt="+imgID
       })
     }
