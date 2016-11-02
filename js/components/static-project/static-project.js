022b58ceb81cda53cb99bad30977f19a524f331a
FazioNico <contact@nicolasfazio.ch>
2016-11-02 13:21:36 +0100

    upd static-project class

--- js/components/static-project/static-project.js
+++ js/components/static-project/static-project.js
@@ -3,13 +3,56 @@ export class StaticProject{
 
   constructor(){
     console.log('Hello static project!')
+    this.selector = document.getElementById('controle-project_type');
+    this.localFolder = 'projects-eleves'
     this.start();
   }
 
   start(){
     let $input = jQuery('input[name=url_projet]')
-    console.log('->', $input.val())
-    document.getElementById('controle-project_type').innerHTML = 'static';
+    if(!$input.val()){
+      this.selector.innerHTML = this.defaultSeleletonUI();
+    }
+    else {
+      console.log('->', $input.val())
+      this.localFolder = this.localFolder + '/userFolder'
+      this.selector.innerHTML = this.SeleletonUI();
+    }
 
   }
+
+
+  /* Class View Methode */
+  defaultSeleletonUI(){
+    return `
+      <p>
+        Le projet de l'élève n'a pas encore copié.<br/>
+        Copier le dossier distant du projet de l'élève dans le répértoir des projets copier:
+      </p>
+      <input style="width: 100%;" type="text" name="distantURL" id="distantURL" value="" placeholder="http://ateliers.nomades.ch/~eleve/dossier-de-projet/"><br/>
+      <button id="copyDistantFolder">Copier le projet de l'élève</button>
+    `;
+  }
+
+  SeleletonUI(){
+    return `
+      <p>
+        Dossier du projet de l'élève: <a href="../../${this.localFolder}" target="_blank">Projet de l'élève</a> <br/>
+      </p>
+      <button id="deleteLocalFolder">supprimer le dossier de l'élève</button>
+    `;
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//
